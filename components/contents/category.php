54851a0c4ca0bb123197acd31f00a2be1669d78d
Andrey_Smetanin <cmutyxa@gmail.com>
2024-09-06 07:31:23 +0300

    styling page category

--- components/contents/category.php
+++ components/contents/category.php
@@ -1,23 +1,28 @@
 <?php
-    $query = "SELECT * FROM products";
-    $res = mysqli_query($conn, $query);
+    //$query = "SELECT * FROM products";
+    //$res = mysqli_query($conn, $query);
 ?>
-<content class="products">
-    <h2></h2>
-<div class="products">
+<content class="page-products">
+    <h2>Столы</h2>
+<div class="product">
     <div class="image-description">
         <img src="Images/Boss_one.jpg" alt="Boss_one">
         <div class="characteristic">
-            <p>Страна производитель:</p>
-            <p>Материал:</p>
-            <p>Цвет:</p>
-            <p>Габариты:</p>
+            <p>Страна производитель: Россия</p>
+            <p>Материал: ЛДСП</p>
+            <p>Цвет: Крафт табачный, серый графит</p>
+            <p>Габариты: 750x900x900</p>
         </div>
         <div class="description">
-
+            Стол Boss One уникален своей лаконичностью и универсальностью, позволит 
+            с комфортом рассадить гостей. При необходимости овальная столешница легко 
+            раздвигается, пространство между половинами стола занимает хранящаяся в 
+            подстолье вставка.Благодаря металлической центральной опоре и правильным 
+            пропорциям основания модель устойчива в разложенном состоянии.
         </div>
-        <div class="btn-details-buy">
-
-        </div>
+    </div>
+    <div class="btn-details-buy">
+        <a href="#" class="btn-deatais">Подробнее</a>
+        <a href="#" class="btn-deatais">Купить</a>
     </div>
 </div>
