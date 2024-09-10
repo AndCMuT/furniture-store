8f6e09c6a0537f8aa06c383f51debf0f3d5e377e
Andrey_Smetanin <cmutyxa@gmail.com>
2024-09-10 08:08:07 +0300

    styling page category, connect bd

--- components/contents/category.php
+++ components/contents/category.php
@@ -1,28 +1,36 @@
 <?php
-    //$query = "SELECT * FROM products";
-    //$res = mysqli_query($conn, $query);
+    $query = "SELECT * FROM products AS prts 
+                JOIN countries ON prts.Country_ID=countries.Country_ID
+                JOIN materials ON prts.Material_ID=materials.Material_ID
+                JOIN colors ON prts.Color_ID=colors.Color_ID
+                JOIN Prices ON prts.Product_ID=Prices.Product_ID
+                WHERE date_start <= CURRENT_TIMESTAMP
+                AND date_start = (SELECT MAX(date_start) FROM Prices WHERE Product_ID=prts.Product_ID)";
+    $res = mysqli_query($conn, $query);
 ?>
 <content class="page-products">
     <h2>Столы</h2>
-<div class="product">
-    <div class="image-description">
-        <img src="Images/Boss_one.jpg" alt="Boss_one">
-        <div class="characteristic">
-            <p>Страна производитель: Россия</p>
-            <p>Материал: ЛДСП</p>
-            <p>Цвет: Крафт табачный, серый графит</p>
-            <p>Габариты: 750x900x900</p>
+<?php
+    while ($row = mysqli_fetch_array($res))
+    $output = "<div class='product'>
+        <div class='image-description'>
+            <img src='Images/$row[image_name]' alt='$row[image_name]'>
+            <div class='characteristic'>
+                <p>Страна производитель: $row[Country_name]</p>
+                <p>Материал: $row[Material_name]</p>
+                <p>Цвет: $row[Color_name]</p>
+                <p>Габариты: $row[width]x$row[depth]x$row[height]</p>
+            </div>
+            <div class='description'>
+                <p><b>$row[Product_name]</b></p>
+                $row[Description]
+            </div>
         </div>
-        <div class="description">
-            Стол Boss One уникален своей лаконичностью и универсальностью, позволит 
-            с комфортом рассадить гостей. При необходимости овальная столешница легко 
-            раздвигается, пространство между половинами стола занимает хранящаяся в 
-            подстолье вставка.Благодаря металлической центральной опоре и правильным 
-            пропорциям основания модель устойчива в разложенном состоянии.
+        <div class='btn-details-buy'>
+            <p>$row[рrice] руб</p>
+            <a href='#' class='btn-deatais'>Купить</a>
+            <a href='#' class='btn-deatais'>Подробнее</a>
         </div>
-    </div>
-    <div class="btn-details-buy">
-        <a href="#" class="btn-deatais">Подробнее</a>
-        <a href="#" class="btn-deatais">Купить</a>
-    </div>
-</div>
+    </div>";
+    print_r($output);
+?>
